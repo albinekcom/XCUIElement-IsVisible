3f63eb0888248a03aecfc118322d9119400221fd
Albin albinek Sadowski <albinek@albinek.com>
2016-10-02 14:45:07 +0200

    Make public variable instead of extension

--- Sources/XCUIElement+IsVisible.swift
+++ Sources/XCUIElement+IsVisible.swift
@@ -3,9 +3,9 @@ import XCTest
 
 @available(iOS 9.0, OSX 10.11, *)
 
-public extension XCUIElement {
+extension XCUIElement {
 
-    var isVisible: Bool {
+    public var isVisible: Bool {
         return exists && !frame.isEmpty && XCUIApplication().windows.element(boundBy: 0).frame.contains(frame)
     }
 
